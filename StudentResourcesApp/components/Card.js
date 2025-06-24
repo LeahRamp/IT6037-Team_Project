65622e093f68fee64852ce195376b37896446bd0
LeahRamp <161538918+LeahRamp@users.noreply.github.com>
2025-06-24 14:31:08 +1200

    Creating card component

--- StudentResourcesApp/components/Card.js
+++ StudentResourcesApp/components/Card.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Text, StyleSheet, Pressable } from 'react-native';
 
-const Card = () => {
+const Card = ({ title, about, onPress }) => {
   return (
-    <View>
-      <Text>Card component</Text>
-    </View>
-  )
-}
+    <Pressable style={styles.card} onPress={onPress}>
+      <Text style={styles.cardTitle}>{title}</Text>
+      <Text style={styles.about} numberOfLines={3} ellipsizeMode="tail">
+        {about}
+      </Text>
+    </Pressable>
+  );
+};
 
 export default Card
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    width: '48%', 
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+    marginBottom: 15,
+  },
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 8,
+    paddingLeft: 10,
+  },
+  about: {
+    fontSize: 14,
+    color: '#666',
+  },
+})
