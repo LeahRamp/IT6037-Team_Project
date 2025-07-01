85e064551c02566118240b75bd3db7378e8865ba
abhy <abinashvj1221@gmail.com>
2025-07-01 12:31:33 +1200

    update tech view

---
+++ app/StudentResourcesApp/screens/ViewTechnology.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+const ViewTechnology = ({ route }) => {
+const { technology } = route.params;
+
+return (
+    <ScrollView contentContainerStyle={styles.container}>
+  <Text style={styles.label}>Title:</Text>
+  <Text style={styles.title}>{technology.Title}</Text>
+  <text></text>
+  <Text style={styles.label}>Type:</Text>
+  <Text style={styles.meta}>{technology.Type}</Text>
+
+  <Text style={styles.label}>Born:</Text>
+  <Text style={styles.meta}>{technology.Born}</Text>
+
+  <Text style={styles.label}>Died:</Text>
+  <Text style={styles.meta}>{technology.Died}</Text>
+
+  <Text style={styles.label}>Nationality:</Text>
+  <Text style={styles.meta}>{technology.Nationality}</Text>
+
+  <Text style={styles.label}>Known For:</Text>
+  <Text style={styles.meta}>{technology.KnownFor}</Text>
+
+  <Text style={styles.label}>About:</Text>
+  <Text style={styles.about}>{technology.About}</Text>
+
+  <Text style={styles.label}>ID:</Text>
+  <Text style={styles.meta}>{technology._id}</Text>
+  
+  <Text style={styles.label}>Designed by:</Text>
+  <Text style={styles.meta}>{technology.DesignedBy }</Text>
+
+  <Text style={styles.label}>Developer:</Text>
+  <Text style={styles.meta}>{technology.Developer}</Text>
+</ScrollView>
+
+);
+};
+
+export default ViewTechnology;
+
+const styles = StyleSheet.create({
+container: {
+padding: 20,
+backgroundColor: '#fff',
+},
+title: {
+fontSize: 24,
+fontWeight: 'bold',
+marginBottom: 15,
+},
+label: {
+fontSize: 16,
+fontWeight: '600',
+marginTop: 10,
+},
+meta: {
+fontSize: 15,
+color: '#333',
+},
+about: {
+fontSize: 15,
+color: '#555',
+marginBottom: 20,
+},
+});
