5699352b5d4f145af43b97b00315fbea8c324359
LeahRamp <161538918+LeahRamp@users.noreply.github.com>
2025-07-01 14:04:34 +1200

    Changed app.sj

--- app/StudentResourcesApp/App.js
+++ app/StudentResourcesApp/App.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Login from './screens/Login';
-import AdminPage from './screens/AdminPage';
-import TutorPage from './screens/TutorPage';
+// import Login from './screens/Login';
+// import AdminPage from './screens/AdminPage';
+// import TutorPage from './screens/TutorPage';
 import Home from './screens/Home';
 import Art from './screens/Art';
 import Technology from './screens/Technology';
@@ -19,14 +19,14 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen 
+      <Stack.Navigator initialRouteName="Home">
+        {/* <Stack.Screen 
           name="Login" 
           component={Login} 
           options={{ headerShown: false }} // hide header on login screen
         />
         <Stack.Screen name="AdminPage" component={AdminPage} />
-        <Stack.Screen name="TutorPage" component={TutorPage} />
+        <Stack.Screen name="TutorPage" component={TutorPage} /> */}
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Art" component={Art} />
         <Stack.Screen name="ViewArt" component={ViewArt} options={{ title: 'Art Details' }} />
