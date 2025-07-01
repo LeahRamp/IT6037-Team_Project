ed4d6b9b0cd9769660d55037ac1a4b90147fd45d
Guruprasaath29 <guruprasaath14@gmail.com>
2025-07-01 16:05:36 +1200

    updating

---
+++ app/StudentResourcesApp/screens/AddMathematics.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddMathematics = () => {
+  return (
+    <View>
+      <Text>AddMathematics</Text>
+    </View>
+  )
+}
+
+export default AddMathematics
